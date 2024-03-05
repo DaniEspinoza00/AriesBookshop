6b55d2ee47d7a4a7676d01e1e33c030d79c29c5d
JorgeDanielEspinoza <jorgedanielespinoza00@gmail.com>
2024-03-05 14:13:40 -0300

    changes in catalogue.component

--- src/app/shared/filters/filters.component.ts
+++ src/app/shared/filters/filters.component.ts
@@ -1,22 +1,68 @@
-import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { book } from './../../interfaces/book';
+import { BooksApiServiceService } from './../../services/books-api-service.service';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {MatSliderModule} from '@angular/material/slider';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [NgxSliderModule,FormsModule],
+  imports: [FormsModule,MatSliderModule,NgFor],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
-export class FiltersComponent {
-  minValue: number = 0;
-  maxValue: number = 50;
-  options: Options = {
-    floor: 0,
-    ceil: 100,
-    step: 1,
-    enforceStep: false,
-    enforceRange: false,
-  };
+export class FiltersComponent implements OnInit{
+
+  bookList:book[]|undefined=[];
+  genreList:String[]=[];
+  authorsList:String[]=[];
+  
+  private router=inject(Router)
+  private BooksApiServiceService=inject(BooksApiServiceService);
+
+  ngOnInit(): void {
+    this.getGenres();
+  }
+
+  filter(event:Event):void{
+    const target=event.target as HTMLSelectElement;
+    const genre = target.value;
+
+    if(genre){
+      this.router.navigate(['/genre',genre]);
+    }
+  }
+
+  getGenres(){
+    this.BooksApiServiceService.getBooks()
+    .subscribe(
+      {
+        next:(books)=>{
+          this.bookList=books;
+          if(this.bookList){
+            const genreSet = new Set <string>();
+            this.bookList.forEach(books=>{
+              const genreArray = books.genres.split(',');
+              genreArray.forEach(genre=>genreSet.add(genre.trim()))
+            });
+            this.genreList=Array.from(genreSet);
+            this.getAutoresHttp();
+          }
+        },
+        error:(error)=>{
+          console.log(error);
+        }
+      }
+    )
+  }
+  getAutoresHttp(){
+    const authorsArray= this.bookList?.map(book=>book.authors);
+    this.authorsList=[... new Set(authorsArray)];
+    console.log(this.authorsList);
+  }
+
+  
 }
