b5d03b8b50327f04282dcecf6e7327ecdac73c52
JorgeDanielEspinoza <jorgedanielespinoza00@gmail.com>
2024-03-05 22:14:39 -0300

    some modifications

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { book } from '../../interfaces/book';
+import { BooksApiServiceService } from './../../services/books-api-service.service';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  private BooksApiServiceService = inject(BooksApiServiceService);
+  bookList:book[]|undefined[]=[];
+  randomIndex:number=0;
+
+  ngOnInit(): void {
+
+    this.showBooksHomePage();
+  }
+
+  showBooksHomePage(){
+    this.BooksApiServiceService.getBooks()
+    .subscribe(
+      {
+        next:(books)=>{
+          /* for(let i=0; i<3;i++){
+            this.randomIndex=Math.floor(Math.random()*books.length);
+          }
+           */
+          
+        },
+        error:(error)=>{
+          console.log(error);
+        }
+      }
+    )
+  }
+
 }
