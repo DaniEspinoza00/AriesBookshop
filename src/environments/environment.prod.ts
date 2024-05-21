5128d3b432c648cd31672c0b03fe719d157d34d3
DaniEspinoza00 <jorgedanielespinoza00@gmail.com>
2024-05-21 16:25:01 -0300

    prod commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+   production:true,
+   baseUrl:'https://example-data.draftbit.com',
+   urlHost:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/',
+   urlApi: 'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/api/v1/',
+   urlBooks:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/api/booklist',
+   urlFavs:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/api/favorites',
+   urlSales:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/api/sales',
+   urlApiKey:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/api/keys', 
+}
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,15 +1,8 @@
 export const environments = {
     production: false,
     baseUrl:'https://example-data.draftbit.com',
-    /* database */
-    /*urlHost:'http://localhost:8080/',
-    urlApi: 'http://localhost:8080/api/v1/',
-    urlBooks:'http://localhost:8080/api/booklist',
-    urlFavs:'http://localhost:8080/api/favorites',
-    urlSales:'http://localhost:8080/api/sales',
-    urlApiKey:'http://localhost:8080/api/keys',*/
 
-     urlHost:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/',
+    urlHost:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/',
     urlApi: 'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/api/v1/',
     urlBooks:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/api/booklist',
     urlFavs:'http://ariesbookshop-env.eba-fgdmagzd.us-east-2.elasticbeanstalk.com/api/favorites',
