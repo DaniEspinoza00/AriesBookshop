e200554c4c95a0a4fd2aefc234e72578bcd0bd7a
DaniEspinoza00 <jorgedanielespinoza00@gmail.com>
2024-05-25 13:51:07 -0300

    environment changes

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
     baseUrl:'https://example-data.draftbit.com',
-    urlHost:'http://ariesbookshop.azurewebsites.net/',
-   urlApi: 'http://ariesbookshop.azurewebsites.net/api/v1/',
-   urlBooks:'http://ariesbookshop.azurewebsites.net/api/booklist',
-   urlFavs:'http://ariesbookshop.azurewebsites.net/api/favorites',
-   urlSales:'http://ariesbookshop.azurewebsites.net/api/sales',
-   urlApiKey:'http://ariesbookshop.azurewebsites.net/api/keys',
+    urlHost:'https://ariesbookshop.azurewebsites.net/',
+   urlApi: 'https://ariesbookshop.azurewebsites.net/api/v1/',
+   urlBooks:'https://ariesbookshop.azurewebsites.net/api/booklist',
+   urlFavs:'https://ariesbookshop.azurewebsites.net/api/favorites',
+   urlSales:'https://ariesbookshop.azurewebsites.net/api/sales',
+   urlApiKey:'https://ariesbookshop.azurewebsites.net/api/keys',
 }
