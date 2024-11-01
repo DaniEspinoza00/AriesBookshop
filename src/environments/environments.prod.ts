f37b31e0a73d6769ec5a88642522ccba9822b893
DaniEspinoza00 <jorgedanielespinoza00@gmail.com>
2024-11-01 01:15:56 -0300

    new envs

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
    production: true,
    baseUrl: 'https://example-data.draftbit.com',
-   urlHost: 'https://aries-bookshop.up.railway.app/',
-   urlApi: 'https://aries-bookshop.up.railway.app/api/v1/',
-   urlBooks: 'https://aries-bookshop.up.railway.app/api/booklist',
-   urlFavs: 'https://aries-bookshop.up.railway.app/api/favorites',
-   urlSales: 'https://aries-bookshop.up.railway.app/api/sales',
-   urlApiKey: 'https://aries-bookshop.up.railway.app/api/keys',
+   urlHost: 'https://ariesbackend.up.railway.app/',
+   urlApi: 'https://ariesbackend.up.railway.app/api/v1/',
+   urlBooks: 'https://ariesbackend.up.railway.app/api/booklist',
+   urlFavs: 'https://ariesbackend.up.railway.app/api/favorites',
+   urlSales: 'https://ariesbackend.up.railway.app/api/sales',
+   urlApiKey: 'https://ariesbackend.up.railway.app/api/keys',
 }
