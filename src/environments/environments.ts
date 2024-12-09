5d73e7b3c1d723e14a2e1dce960246a4c521758e
DaniEspinoza00 <jorgedanielespinoza00@gmail.com>
2024-12-08 22:46:52 -0300

    new database

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
     baseUrl: 'https://example-data.draftbit.com',
-    urlHost: 'https://ariesbackend.up.railway.app/',
-    urlApi: 'https://ariesbackend.up.railway.app/api/v1/',
-    urlBooks: 'https://ariesbackend.up.railway.app/api/booklist',
-    urlFavs: 'https://ariesbackend.up.railway.app/api/favorites',
-    urlSales: 'https://ariesbackend.up.railway.app/api/sales',
-    urlApiKey: 'https://ariesbackend.up.railway.app/api/keys',
+    urlHost: 'https://aries-bookshop-backend.onrender.com/',
+    urlApi: 'https://aries-bookshop-backend.onrender.com/api/v1/',
+    urlBooks: 'https://aries-bookshop-backend.onrender.com/api/booklist',
+    urlFavs: 'https://aries-bookshop-backend.onrender.com/api/favorites',
+    urlSales: 'https://aries-bookshop-backend.onrender.com/api/sales',
+    urlApiKey: 'https://aries-bookshop-backend.onrender.com/api/keys',
 }
